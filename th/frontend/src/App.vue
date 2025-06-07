4e5e28412df4ce79fae3c136369232ce09dd8831
ctv992005 <vyctps41133@gmail.com>
2025-06-07 23:28:07 +0700

    nhanh phu

--- th/frontend/src/App.vue
+++ th/frontend/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div id="app">
-    <nav class="nav-main">
-      <router-link class="a" to="/">Trang chủ </router-link> 
-      <router-link  class="a" to="/login">Đăng nhập </router-link> 
-      <router-link  class="a" to="/register">Đăng ký</router-link>
-      <router-link  class="a" to="/admin">Quản trị</router-link>
+  <div id="app"> 
+    <nav class="nav-main" v-if="!isAdminRoute">
+      <router-link class="a" to="/">Trang chủ</router-link>
+      <router-link class="a" to="/login">Đăng nhập</router-link>
+      <router-link class="a" to="/register">Đăng ký</router-link>
+      
     </nav>
     <router-view />
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+
+const isAdminRoute = computed(() => route.path.startsWith('/admin'))
 </script>
 
 <style>
